5983c3f4170c40368b606dc09025e6378da15a7a
“Philipp <philipp.diethard@fprimezero.com>
2022-03-29 18:01:30 +0200

    WIP 1.2

--- Ass1/denoising.py
+++ Ass1/denoising.py
@@ -25,9 +25,73 @@ def task2():
 
     """ Start of your code
     """
-    h1 = None # don't forget to set the 3 chosen values h1,h2,h3 in your code
-    h2 = None
-    h3 = None
+
+    # 1.
+    means = [[0, 0], [0,1.5], [1.5,1.5]]
+    cov = [[0.075, 0], [0, 0.075]]
+    sample_size = 300
+    y = []
+    x = []
+    for mean in means:
+        x_train, y_train = np.random.multivariate_normal(mean, cov, sample_size).T
+
+        x.extend(x_train)
+        y.extend(y_train)
+
+    y = np.array([x,y]).T
+    """
+     if w is None:
+        w = x.new_ones(x.shape[0])
+    else:
+        w = x.shape[0] * w
+
+    d = x.data.size()[1] + 1
+    n_s = x_i.data.size()[0]
+
+    term_in_brackets = th.exp(-th.cdist(x, x_i) ** 2 / (2 * h ** 2))
+    sum = th.sum(w * term_in_brackets, dim=1)
+    fraction_term = 1 / (math.pow((2 * math.pi), (d / 2)) * n_s * math.pow(h, d))
+    
+    """
+
+
+    # 2.
+    #link for report: https://aakinshin.net/posts/kde-bw/
+    h1 = 0.05  # don't forget to set the 3 chosen values h1,h2,h3 in your code
+    h2 = 0.5
+    h3 = 1
+
+    N = sample_size * len(means)
+    D = 2
+    kde_list = []
+    for h in [h1,h2,h3]:
+        sum = 0
+        f_kde = np.zeros_like(y)
+        for n in range(0, N):
+            """y_n = y[n]
+            term_1 = (1 / np.power(np.sqrt(2 * np.pi) * h, D))
+            dist = np.array([np.linalg.norm(i - y_n) for i in y])
+            term_2 = np.exp(-dist ** 2 / (2 * h ** 2))
+            sum += term_1 * term_2"""
+            tmp = (y[n] - y) / h
+            kernel = np.exp(-0.5 * tmp ** 2) / (np.sqrt(2 * np.pi) * h)
+            f_kde[n] = kernel.sum() / (y.shape[0])
+        kde = f_kde
+        kde_list.append(kde)
+
+    tmp_1, tmp_2 = zip(*y)
+    ax[0].scatter(tmp_1,tmp_2)
+    for i in  range(0, len(kde_list)):
+        #ax[i+1].plot(y,kde_list[i])
+        tmp_1, tmp_2 = zip(*kde_list[i])
+        ax[i+1].hist2d(tmp_1, tmp_2)
+
+
+    x_test = [0,1.5]
+
+    for n in range (0, N):
+        y_n = y[n]
+
 
     """ End of your code
     """
@@ -65,7 +129,7 @@ def task3():
 
     """ Start of your code
     """
-    
+
     """ End of your code
     """
     return fig
